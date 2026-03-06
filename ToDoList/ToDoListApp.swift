c4bb824c215e8a09a122983a0a8cb2d7f6f75020
Nick Collins <necollins03@gmail.com>
2026-03-06 13:11:51 -0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Nick Collins on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
